400f833200f2ba81be5f8adf401d8bd246843f84
Johan Xie <johanpoisson@outlook.com>
2023-02-01 13:41:33 +0800

    feat(colorscheme): modify the `catppuccin` colorscheme

--- init.lua
+++ init.lua
@@ -8,9 +8,5 @@ require("core.keymaps")
 require("core.lazy")
 
 vim.cmd("colorscheme catppuccin")
--- Disable cursorline highlight
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
--- Make comment more eye-catching
-vim.api.nvim_set_hl(0, "Comment", { fg = "#eba0ac" })
 
 -- vim:sw=2:ts=2:sts=2:et:tw=80:cc=+1:norl:
