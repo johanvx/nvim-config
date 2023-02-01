0a8ba7df2609e8b6963d59300047649cbee85425
Johan Xie <johanpoisson@outlook.com>
2023-02-01 17:39:14 +0800

    fix(python-provider): add support for micromamba

--- lua/core/envs.lua
+++ lua/core/envs.lua
@@ -1,7 +1,9 @@
 -- Python
-if vim.env["CONDA_PREFIX"] then
-  -- Use an independent environment `pynvim` for Neovim
-  local base_prefix = (vim.env["CONDA_PREFIX"]):match("^.-base")
+-- Use an independent conda environment `pynvim` for Neovim
+local conda_prefix = vim.env["CONDA_PREFIX"]
+if conda_prefix then
+  -- Use `conda_prefix` as fallback for micromamba
+  local base_prefix = conda_prefix:match("^.-base") or conda_prefix
   local pynvim_python =
     User.fn.path_concat(base_prefix, "envs", "pynvim", "bin", "python")
   vim.g.python3_host_prog = pynvim_python
