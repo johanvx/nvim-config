30b29284a3a0d7592e426869760119b6deb30c23
Johan Xie <johanpoisson@outlook.com>
2023-03-26 23:50:53 +0800

    feat!(mini.surround): use default settings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -246,18 +246,6 @@ return {
   {
     "echasnovski/mini.surround",
     event = "VeryLazy",
-    opts = {
-      search_method = "cover_or_next",
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
     config = function(_, opts)
       require("mini.surround").setup(opts)
     end,
