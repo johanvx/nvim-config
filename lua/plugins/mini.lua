c3166204fd974e9ed876f8518ced8af9d90ed70d
Johan Xie <johanpoisson@outlook.com>
2023-03-27 19:44:00 +0800

    feat(mini.completion): filter some items and disable borders

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -76,11 +76,13 @@ return {
     opts = {
       lsp_completion = {
         source_func = "omnifunc",
-        auto_setup = false,
-      },
-      window = {
-        info = { border = "rounded" },
-        signature = { border = "rounded" },
+        process_items = function(items, base)
+          -- Don't show 'Text' and 'Snippet' suggestions
+          items = vim.tbl_filter(function(x)
+            return x.kind ~= 1 and x.kind ~= 15
+          end, items)
+          return MiniCompletion.default_process_items(items, base)
+        end,
       },
     },
     config = function(_, opts)
