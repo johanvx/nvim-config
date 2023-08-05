75a13974013e8b8b29104ea03efe25822a6d6dc6
Johan Xie <johanpoisson@outlook.com>
2023-08-05 13:50:44 +0800

    feat(lsp): update LSP configurations

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,53 @@
 -- Plugin-related table
 User.p = {}
 
+function User.p.format_and_save()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ft = vim.bo[bufnr].filetype
+  local has_null_ls = #require("null-ls.sources").get_available(
+    ft,
+    "NULL_LS_FORMATTING"
+  ) > 0
+
+  -- Format
+  vim.lsp.buf.format({
+    bufnr = bufnr,
+    filter = function(client)
+      return has_null_ls == (client.name == "null-ls")
+    end,
+  })
+
+  -- Save
+  vim.cmd("update")
+end
+
+function User.p.server_opts_with_fallback(opts)
+  return vim.tbl_deep_extend("keep", opts, {
+    on_attach = function(client, bufnr)
+      -- Use echasnovski/mini.completion
+      -- vim.bo[bufnr].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
+
+      -- Use SmiteshP/nvim-navic
+      if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, bufnr)
+      end
+
+      -- Enable inlay hint
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint(bufnr, true)
+      end
+
+      -- Use ray-x/lsp_signature.nvim
+      require("lsp_signature").on_attach({
+        doc_lines = 0,
+        handler_opts = {
+          border = "none",
+        },
+      })
+    end,
+    -- Use hrsh7th/cmp-nvim-lsp for hrsh7th/nvim-cmp
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  })
+end
+
 return {}
--- lua/plugins/lang-ft.lua
+++ lua/plugins/lang-ft.lua
@@ -1,7 +1,7 @@
 -- This is for specific languages and filetypes.
 
 return {
-  -- Markdown
+  -- Requires `deno` for Markdown
   {
     "toppair/peek.nvim",
     build = "deno task --quiet build:fast",
@@ -26,6 +26,51 @@ return {
     config = function(_, opts)
       require("peek").setup(opts)
     end,
+  },
+  -- Requires `rust-analyzer` for Rust
+  --
+  -- This plugin sets up the `servers.rust_analyzer` for `nvim-lspconfig`, so
+  -- don't do that manually in `lsp.lua`.
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    opts = { server = User.p.server_opts_with_fallback({}) },
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  -- A `tsserver` alternative for TypeScript
+  --
+  -- This plugin acts as a replacement of `servers.tsserver` for
+  -- `nvim-lspconfig`, so don't set up `tsserver` manually in `lsp.lua`.
+  {
+    "pmizio/typescript-tools.nvim",
+    enabled = false,
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = User.p.server_opts_with_fallback({
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
+    }),
+    config = function(_, opts)
+      require("typescript-tools").setup(opts)
+    end,
+  },
+  -- Typst support
+  {
+    "kaarmu/typst.vim",
+    ft = "typst",
   },
 }
 
