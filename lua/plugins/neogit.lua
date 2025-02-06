4686546ae0a0d8e302996958110f663a769872e2
Johan Xie <johanpoisson@outlook.com>
2025-02-06 14:07:44 +0800

    feat(plugin): add neogit for integrated Git interface
    Note that mini-git is another available tool to use Git, in a CLI-like
    way.

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "echasnovski/mini.pick",
+    },
+    config = true,
+  },
+}
+-- vim:sw=2:ts=2:sts=2:et:tw=80:cc=+1:norl:
