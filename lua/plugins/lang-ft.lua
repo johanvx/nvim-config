97a42350f2f922dc0fc9c78cc03adae2c266da00
Johan Xie <johanpoisson@outlook.com>
2025-02-11 17:55:47 +0800

    feat(lang-ft): drop yarn requirement for markdown-preview

--- lua/plugins/lang-ft.lua
+++ lua/plugins/lang-ft.lua
@@ -1,12 +1,14 @@
 -- This is for specific languages and filetypes.
 
 return {
-  -- Requires `yarn` for Markdown
+  -- For Markdown
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = "cd app && yarn install",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
     keys = {
       {
         "<LocalLeader>p",
