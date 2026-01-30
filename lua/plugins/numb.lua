d196a1bcc811c35e60e200b1bf457502e7224c82
Johan Xie <johanpoisson@outlook.com>
2026-01-30 21:26:46 +0800

    good for peeking lines

---
+++ lua/plugins/numb.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nacro90/numb.nvim",
+    opts = {
+      show_numbers = true,
+      show_cursorline = true,
+      hide_relativenumbers = true,
+      number_only = false,
+      centered_peeking = true,
+    },
+  },
+}
+
+-- vim:sw=2:ts=2:sts=2:et:tw=80:cc=+1:norl:
